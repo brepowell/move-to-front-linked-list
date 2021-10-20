a01b6605ffb0fbe1a95633c7f30acd4530fb98b5
brepowell <85724630+brepowell@users.noreply.github.com>
2021-10-19 21:08:45 -0700

    Testing

---
+++ MTFList.cpp
@@ -0,0 +1,32 @@
+// Created by Breanna Powell
+// 501 B / FALL
+// SID: 0641962
+// Due: 2021 10 20
+
+//** MTFList */
+#include <iostream>
+#include "LinkedList.h"
+#include "MTFList.h"
+using namespace std;
+
+//Default Constructor
+MTFList::MTFList()
+   : head(nullptr), listSize(0)
+{
+}//end default constructor         
+
+//Destructor
+MTFList::~MTFList()
+{
+   clear(); //How do I get this to work? LinkedList:: clear();
+}//end destructor
+
+/** Tests whether the list contains a given entry.
+ Overrides contains from LinkedList.h
+ @post If the entry is found, the node is moved to the front of the list.
+ @param anEntry  The entry to locate.
+ @return  True if list contains anEntry, or false if not. */
+bool MTFList::contains(int anEntry){
+
+
+}//end contains
